5a18257a1a36a062661c840dcd1862a38e631261
Jose Jaramillo <Jose@JosePc>
2021-01-12 09:59:36 -0500

    Cambios a los fxml para el menu 2

--- src/main/java/app/ConfigController.java
+++ src/main/java/app/ConfigController.java
@@ -4,20 +4,22 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-
+import javafx.scene.control.CheckBox;
 public class ConfigController {
     
-    @FXML ComboBox opcionVisible;
-    @FXML ComboBox cantidadOpcion;
+    @FXML CheckBox checkVisible;
+    @FXML ComboBox<String> comboOpcion;
     @FXML Button botonReporte;
     
     @FXML
     private void switchToPrimary() throws IOException {
         App.setRoot("primary");
     }
-    
 
-
-    
-    
+    public void initialize() throws IOException{
+        comboOpcion.getItems().addAll("1 Oponente","2 Oponentes");
+        botonReporte.setOnMouseClicked(ev ->{
+            App.setRoot("primary");
+        });
+    }
 }
